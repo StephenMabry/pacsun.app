2f621cf43121e81b46591142d399e6f97eb53986
StephenMabry <sm00010@mix.wvu.edu>
2022-02-02 17:59:51 -0500

    Lab Assignment 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CategoryCardComponent } from './categories/category.card.component';
 import { BottomNavComponent } from './navigation/bottomnav.component';
 import { TopNavBarComponent } from './navigation/topnavbar.component';
+import { PromotionsCarouselComponent } from './promotions/promotions.carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopNavBarComponent,
-    BottomNavComponent
+    BottomNavComponent,
+    PromotionsCarouselComponent,
+    CategoryCardComponent,
+
   ],
   imports: [
     BrowserModule
---
+++ src/app/promotions/promotions.carousel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'ps-promotions-carousel',
+    templateUrl: 'promotions.carousel.component.html',
+    styleUrls: ['promotions.carousel.component.css']
+})
+
+export class PromotionsCarouselComponent{
+
+}
